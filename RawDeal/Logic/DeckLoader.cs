d0014588b4c0df0360f68747ea393b38a2b37b00
Aníbal Contreras <anibal.contreras@uc.cl>
2023-09-03 13:48:03 -0300

    Feat - Editing logic namespaces files

--- RawDeal/Logic/DeckLoader.cs
+++ RawDeal/Logic/DeckLoader.cs
@@ -1,67 +1,65 @@
 using RawDeal.Models;
 
-namespace RawDeal.Logic
+namespace RawDeal.Logic;
+public static class DeckLoader
 {
-    public static class DeckLoader
+    private static Dictionary<string, Card> allAvailableCards = new Dictionary<string, Card>();
+    private static Dictionary<string, Superstar> allAvailableSuperstars = new Dictionary<string, Superstar>();
+
+    public static void InitializeDeckLoader()
     {
-        private static Dictionary<string, Card> allAvailableCards = new Dictionary<string, Card>();
-        private static Dictionary<string, Superstar> allAvailableSuperstars = new Dictionary<string, Superstar>();
+        var cards = CardLoader.LoadCardsFromJson();
+        var superstars = SuperstarLoader.LoadSuperstarsFromJson();
 
-        public static void InitializeDeckLoader()
+        foreach (var card in cards)
         {
-            var cards = CardLoader.LoadCardsFromJson();
-            var superstars = SuperstarLoader.LoadSuperstarsFromJson();
-
-            foreach (var card in cards)
-            {
-                allAvailableCards[card.Title] = card;
-            }
-
-            foreach (var superstar in superstars)
-            {
-                allAvailableSuperstars[superstar.Name] = superstar;
-            }
-        }
-        public static Deck LoadDeck(string path)
-        {
-            if (!File.Exists(path))
-                throw new FileNotFoundException($"El archivo {path} no fue encontrado.");
-
-            var lines = File.ReadAllLines(path);
-            if (lines.Length == 0)
-                throw new Exception($"El archivo {path} está vacío.");
-
-            var deck = new Deck
-            {
-                Superstar = GetSuperstarByName(lines[0].Replace(" (Superstar Card)", "")),
-                Cards = new List<Card>()
-            };
-
-            for (int i = 1; i < lines.Length; i++)
-            {
-                var cardTitle = lines[i];
-                if (allAvailableCards.ContainsKey(cardTitle))
-                {
-                    deck.Cards.Add(allAvailableCards[cardTitle]);
-                }
-                else
-                {
-                    Console.WriteLine($"Advertencia: La card '{cardTitle}' no se encontró en las cartas disponibles.");
-                }
-            }
-            return deck;
+            allAvailableCards[card.Title] = card;
         }
 
-        private static Superstar GetSuperstarByName(string name)
+        foreach (var superstar in superstars)
         {
-            if (allAvailableSuperstars.ContainsKey(name))
+            allAvailableSuperstars[superstar.Name] = superstar;
+        }
+    }
+    public static Deck LoadDeck(string path)
+    {
+        if (!File.Exists(path))
+            throw new FileNotFoundException($"El archivo {path} no fue encontrado.");
+
+        var lines = File.ReadAllLines(path);
+        if (lines.Length == 0)
+            throw new Exception($"El archivo {path} está vacío.");
+
+        var deck = new Deck
+        {
+            Superstar = GetSuperstarByName(lines[0].Replace(" (Superstar Card)", "")),
+            Cards = new List<Card>()
+        };
+
+        for (int i = 1; i < lines.Length; i++)
+        {
+            var cardTitle = lines[i];
+            if (allAvailableCards.ContainsKey(cardTitle))
             {
-                return allAvailableSuperstars[name];
+                deck.Cards.Add(allAvailableCards[cardTitle]);
             }
             else
             {
-                throw new Exception($"Superstar {name} no encontrado.");
+                Console.WriteLine($"Advertencia: La card '{cardTitle}' no se encontró en las cartas disponibles.");
             }
+        }
+        return deck;
+    }
+
+    private static Superstar GetSuperstarByName(string name)
+    {
+        if (allAvailableSuperstars.ContainsKey(name))
+        {
+            return allAvailableSuperstars[name];
+        }
+        else
+        {
+            throw new Exception($"Superstar {name} no encontrado.");
         }
     }
 }
